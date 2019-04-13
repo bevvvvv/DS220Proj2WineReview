26b03491c80a1e291e320cddde7bd58b3a1a7e73
Voidtex <32623966+Voidtex@users.noreply.github.com>
2019-04-13 18:25:02 -0400

    Set up listed form

--- polls/views.py
+++ polls/views.py
@@ -13,10 +13,13 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s."%question_id)
+    return HttpResponse("You're looking at question %s." % question_id)
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
