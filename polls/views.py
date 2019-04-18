8b54d607e4ba1b4dbb06b0b9f43ffb02a4ea4e72
Andrew Rost <andrewrost0@gmail.com>
2019-04-18 00:11:27 -0400

    New results page for after vote button is clicked

--- polls/views.py
+++ polls/views.py
@@ -6,12 +6,16 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index1.html'
+    template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
+
+
+class WineView(generic.DetailView):
+    template_name = 'polls/wineresults.html'
 
 
 class DetailView(generic.DetailView):
