c4121e2527f1873a094776cb308be9a2c2ef0db3
Voidtex <32623966+Voidtex@users.noreply.github.com>
2019-04-22 19:55:31 -0400

    Initialized options

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request
+import pandas as pd
+#import queryFunctions
 
 app = Flask(__name__)
 host = 'http://127.0.0.1:5000/'
+wineTypes = sorted(pd.read_csv('wines.csv')['x'].dropna().tolist())
+countries = sorted(pd.read_csv('countries.csv')['x'].dropna().tolist())
+wineries = sorted(pd.read_csv('wineries.csv')['x'].dropna().tolist())
+regions = sorted(pd.read_csv('regions.csv')['x'].dropna().tolist())
 
 @app.route('/', methods=['POST', 'GET'])
 def poll():
-    
-    return render_template('index.html')
+    if request.method == 'POST':
+        pass
+
+    return render_template('index.html', wineTypes=wineTypes, countries=countries, wineries=wineries, regions=regions)
